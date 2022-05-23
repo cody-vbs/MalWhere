edd65ded4163bd2ddc1c42baee2cbedc5ebda689
cody-vbs <ricafortsyd11@gmail.com>
2022-05-23 19:25:26 +0800

    update on splash screen

--- app/src/main/java/com/codyvbs/malwhere/Splash.java
+++ app/src/main/java/com/codyvbs/malwhere/Splash.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.white.progressview.HorizontalProgressView;
+
 public class Splash extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 5000;
+    private HorizontalProgressView horizontalProgressView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        horizontalProgressView = findViewById(R.id.progress100);
+        horizontalProgressView.setProgressInTime(0,100,5000);
 
         new Handler().postDelayed(new Runnable(){
             @Override
